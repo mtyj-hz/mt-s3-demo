5dba071dc6038ee2204fbd744d846a7e45dd4227
yanzq <endelove@dingtalk.com>
2022-05-18 17:07:51 +0800

    feat:share

--- share/link.go
+++ share/link.go
@@ -8,7 +8,9 @@ type Link struct {
 	svc *s3.S3
 }
 
+// Perlink 这只桶的ACl可用通过  http(s)://域名/桶名称/图片名称.jpg直接访问object
 func (l *Link) Perlink(bucket *string) error {
+	// Valid Values: private | public-read | public-read-write | authenticated-read
 	acl := "public-read"
 	l.svc.PutBucketAcl(&s3.PutBucketAclInput{
 		Bucket: bucket,
